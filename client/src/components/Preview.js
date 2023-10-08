a3e3253512b8efe5a3861ef7bacc41e0b09796ba
Illia <illia.rolia@plus4u.net>
2023-10-08 14:42:00 +0200

    leaf-bg Preview

--- client/src/components/Preview.js
+++ client/src/components/Preview.js
@@ -15,7 +15,7 @@ export default function Preview() {
             <div className='preview'>
                 <div className='bg_group'>
                     <img className='bg' src='/images/bg1.svg' alt='leaf' />
-                    <img className='bg' src='/images/bg2.svg' alt='leaf' />
+                    <img className='bg' src='/images/leaf-bg.png' alt='leaf' />
                     <div className='bg_text'>
                         <h1 dangerouslySetInnerHTML={{ __html: t('PreviewH1') }} />
                         <h4>{t('PreviewH4')}</h4>
